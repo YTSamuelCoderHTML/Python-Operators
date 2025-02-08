9d5c989dca65b83a9a30167ddc0becb492bbbf73
YTSamuelCoderHTML <muhammadsaimnaeem481@gmail.com>
2025-02-08 19:28:55 +0500

    Add files via upload

---
+++ not-equal.py
@@ -0,0 +1,23 @@
+a = 7
+b= 10
+c= 9
+
+print(a != b)
+print(a != c)
+
+a = "python"
+b = "coding"
+
+if a != b:
+    print(a, "and", b, "are not same!")
+
+a = 4
+b = 5
+
+if (a == 1) != (b == 5):
+    print("Hello!")
+
+number = int(input("Write a number:"))
+
+if number%2 != 0:
+    print("The number isn't an even number!")
